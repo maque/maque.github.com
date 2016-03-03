338df8877b2ffe1a4bf506b3514173450171564e
maque <marcinmakowski@konto.pl>
2016-03-03 21:38:53 +0100

    code fixes for speed

--- assets/js/jsdemoscene/rotozoom/effect.js
+++ assets/js/jsdemoscene/rotozoom/effect.js
@@ -3,8 +3,8 @@ var IntervalAnimation;
 var _Screen;
 var multipler=0;
 var adder=0.01;
-var width;
-var height;
+var width, halfWidth;
+var height, halfHeight;
 function Init(){
 		
 	var canvas = document.getElementById('screen');
@@ -13,7 +13,8 @@ function Init(){
 
 		width = canvas.width;
 		height = canvas.height;
-		
+		halfWidth = width/2;
+		halfHeight = height/2;
 		_Screen = new Screen(ctx,width,height);
 		
 		GenerateGrayScale();
@@ -49,13 +50,15 @@ function ToggleAnimation(){
 function DrawChecker(multipler){
 	_Screen.FillWithColor(ColorPalette[0]);
 	angle=multipler*Math.PI;
+	cosinus_angle = Math.cos(angle);
+	sinus_angle = Math.sin(angle);
     for(y=0;y<height;y++){
-		 yy=-y+height/2;
+		 center_y=-y+halfHeight; // transform x/y to Cartesian coordinate system
          for(x=0;x<width;x++){
-	        xx=x-width/2;
+	        center_x=x-halfWidth;
 			   
-		    newx=Math.floor(Math.cos(angle)*xx - yy*Math.sin(angle))+xx+320;
-			newy=Math.floor(Math.sin(angle)*xx+yy*Math.cos(angle))+yy+240;
+		    newx=Math.floor(cosinus_angle*center_x - center_y*sinus_angle)+center_x+halfWidth;
+			newy=Math.floor(sinus_angle*center_x + center_y*cosinus_angle)+center_y+halfHeight;
 		
 		
 			index = Math.abs((newx*multipler)^(newy*multipler))%256;
